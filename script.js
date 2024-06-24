c87f7352161bcd98fef402f5ef214175d70077d6
Qajay0832 <123375564+Qajay0832@users.noreply.github.com>
2024-06-24 12:43:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 //your JS code here. If required.
+let text = document.querySelector("#text");
+let delay = document.querySelector("#delay");
+let btn = document.querySelector("#btn");
+let output = document.querySelector("#output");
+
+
+
+function Promise1() {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve(text.value);
+        }, delay.value)
+    })
+}
+async function showOutput() {
+    let res = await Promise1();
+	output.textContent=res;
+}
+
+btn.addEventListener("click", showOutput)
